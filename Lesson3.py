e10895beac6318a1d442e6c2e9d4b9498e219f91
Timur Hisamov <timur.hisamov@gmail.com>
2018-10-16 19:30:54 +0300

    add

--- Lesson3.py
+++ Lesson3.py
@@ -13,3 +13,9 @@ full_name = f_name + " " + s_name
 
 print(f_name + " " + s_name)
 print(full_name + " " + str(age))
+
+num1 = 1111111111111
+num2 = 2222323222211231
+num3 = num1 + num2
+
+print(num1 + num2 )
