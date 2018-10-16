955489029512f4b008b477106a4f0e8e10f280ac
Timur Hisamov <timur.hisamov@gmail.com>
2018-10-16 22:36:37 +0300

    add Lesson5.py

---
+++ Lesson5.py
@@ -0,0 +1,16 @@
+name = "timur hiSAmov"
+
+print ("Lower name: " + name.lower())
+print ("Name: " + name .title())
+
+ex = "  ......MY name is  ....."
+
+print (ex.lstrip())
+print (ex.rstrip( ))
+print (ex.strip())
+print (ex.strip(" . "))
+
+ex = ex.strip()
+ex = ex.strip(".")
+
+print (ex)
